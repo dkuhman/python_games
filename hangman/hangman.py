1f2308343508e8172ea21ff782d35db801575dab
danielkuhman <dkuhman@uab.edu>
2020-02-04 12:31:24 -0600

    Added ability to play multiple games

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -15,47 +15,56 @@ list_of_words = list()
 for line in file_handle:
     list_of_words.append(line.rstrip())
 
-#Select a random word from the list
-game_word = random.choice(list_of_words)
+#Start new game
+game_status = 1
+while game_status == 1:
+    #Select a random word from the list
+    game_word = random.choice(list_of_words)
 
-#Show the user the number of letters in the game word
-display_word = list()
-for i in range(len(game_word)):
-    display_word.append('_ ')
+    #Show the user the number of letters in the game word
+    display_word = list()
+    for i in range(len(game_word)):
+        display_word.append('_ ')
 
-print('Your word:',''.join(display_word))
+    print('Your word:',''.join(display_word))
 
-#Start the game
-list_of_guesses = list()
-misses = 0 #The user gets 6 misses before the game ends
-while misses < 6:
-    user_guess = input('Guess a letter:')
-    user_guess = user_guess.lower()
-    if len(user_guess) > 1:
-        print('Your guess must be a single letter!')
-    else:
-        if user_guess in list_of_guesses: #See if the letter was already guessed
-            print('You already guessed that letter!')
-        elif user_guess.isalpha() == False: #Ensure input is alphabetical
-            print('Your guess must be a letter!')
+    #Start the game
+    list_of_guesses = list()
+    misses = 0 #The user gets 6 misses before the game ends
+    while misses < 6:
+        user_guess = input('Guess a letter:')
+        user_guess = user_guess.lower()
+        if len(user_guess) > 1:
+            print('Your guess must be a single letter!')
         else:
-            list_of_guesses.append(user_guess)
-            if user_guess not in game_word:
-                misses = misses + 1
-                print('Oh no! That letter is not in this word!')
-                print('You have', 6 - misses, 'left!')
-                if misses == 6:
-                    print('Oh no! You lost!')
-                    break
+            if user_guess in list_of_guesses: #See if the letter was already guessed
+                print('You already guessed that letter!')
+            elif user_guess.isalpha() == False: #Ensure input is alphabetical
+                print('Your guess must be a letter!')
             else:
-                print('Nice guess!')
-                #correct_list = list()
-                for match in re.finditer(user_guess,game_word): #Needed to see if the letter appears more than once
-                    display_word[match.start()] = user_guess
-                    print('Your word:',''.join(display_word))
-                if ''.join(display_word) == game_word:
-                    print('Congratulations! You won!')
-                    break
+                list_of_guesses.append(user_guess)
+                if user_guess not in game_word:
+                    misses = misses + 1
+                    print('Oh no! That letter is not in this word!')
+                    print('You have', 6 - misses, 'left!')
+                    if misses == 6:
+                        print('Oh no! You lost!')
+                        print('The game word was', game_word)
+                        break
+                else:
+                    print('Nice guess!')
+                    #correct_list = list()
+                    for match in re.finditer(user_guess,game_word): #Needed to see if the letter appears more than once
+                        display_word[match.start()] = user_guess
+                        print('Your word:',''.join(display_word))
+                    if ''.join(display_word) == game_word:
+                        print('Congratulations! You won!')
+                        break
 
-print('Oh no, you lost!')
-print('The game word was:', game_word)
+    play_again = input('Would you like to keep playing? (y/n): ')
+    play_again = play_again.lower()
+    if play_again == 'y':
+        game_status = 1
+    else:
+        game_status = 0
+        break
